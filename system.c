747c69f82259469ced42ca83962f99e062fb8647
Unai Lizarralde Imaz <totxivica@gmail.com>
2019-10-09 14:46:07 +0200

    RR Schedule included

--- system.c
+++ system.c
@@ -21,12 +21,25 @@ void start_simulation(){
         for(i = 0; i < computing_engine.ncpus; i++){
             for(j = 0; j <  computing_engine.cpus[i].ncores; j++){
                 for(k = 0; k < computing_engine.cpus[i].cores[j].nhthreads; k++){
-                    if(computing_engine.cpus[i].cores[j].hthreads[k].proc->pid == 0){
-                        scheduler(i, j, k);
-                    }else{
-                        computing_engine.cpus[i].cores[j].hthreads[k].proc->cycles--;
-                        if(computing_engine.cpus[i].cores[j].hthreads[k].proc->cycles == 0)
-                            scheduler(i, j, k);
+                    if (conf.execution_policy == NONPREEMPTIVE){
+                        if (conf.scheduling_policy == FIFO){
+                            if(computing_engine.cpus[i].cores[j].hthreads[k].proc->pid == 0){
+                                scheduler(i, j, k);
+                            }else{
+                                computing_engine.cpus[i].cores[j].hthreads[k].proc->cycles--;
+                                if(computing_engine.cpus[i].cores[j].hthreads[k].proc->cycles == 0) scheduler(i, j, k);
+                            }
+                        }
+                    } else {
+                        if (conf.scheduling_policy == RR){
+                            if(computing_engine.cpus[i].cores[j].hthreads[k].proc->pid == 0){
+                                scheduler(i, j, k);
+                            } else {
+                                computing_engine.cpus[i].cores[j].hthreads[k].proc->cycles--;
+                                computing_engine.cpus[i].cores[j].hthreads[k].proc->executed_tick_in_a_row ++;
+                                if(computing_engine.cpus[i].cores[j].hthreads[k].proc->cycles == 0 || computing_engine.cpus[i].cores[j].hthreads[k].proc->executed_tick_in_a_row == conf.quantum) scheduler(i, j, k);
+                            }
+                        }
                     }
                     if(!computing_engine.cpus[i].cores[j].hthreads[k].proc->pid == 0) active++;
                     printf("TICK: %d, CPU %d, CORE: %d, THREAD: %d, PID: %d \n", current_tick, i, j, k, computing_engine.cpus[i].cores[j].hthreads[k].proc->pid);
@@ -34,6 +47,7 @@ void start_simulation(){
             }
         }
         if(active == 0) {
+            printf("All processes have been handled by the system\n");
             exit(0);
         }
     }   
